7762880b57e676632108b43e0f36c7cae0dda14a
rchacon52 <rchacon52@gmail.com>
2024-11-14 21:16:49 -0800

    update4

--- terraform/pre-run.sh
+++ terraform/pre-run.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+
+export HCP_CLIENT_ID=p8eCxXiBVC41ZPxGd6WGtkjJ7bB1y6hw
+export HCP_CLIENT_SECRET=5c3_AkyAFY57LM8jYMwP9dyp6DHQHk7GW3XmN0olokoNm-hVcuO_xbGNSR_aL5ky
+export APP_NAME=WebApplication
 #export TF_LOG=DEBUG
+
+
+hcp profile set vault-secrets/app $APP_NAME
+hcp auth login
+#hcp vault-secrets secrets list --app=$APP_NAME 
+echo " "
 
 echo " "
  echo "Running terraform init"
