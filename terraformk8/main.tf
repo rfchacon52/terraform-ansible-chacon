c7a78d2d06d53351c7347635a6355662fd94211d
rchacon52 <rchacon52@gmail.com>
2024-11-28 06:19:22 -0800

    update

--- terraformk8/main.tf
+++ terraformk8/main.tf
@@ -90,13 +90,6 @@ resource "aws_iam_role_policy_attachment" "additional" {
   role       = each.value.iam_role_name
 }
 
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-    }
-}
 
 resource "helm_release" "ingress" {
   name       = "ingress"
