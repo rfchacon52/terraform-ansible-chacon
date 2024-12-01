d4f17532870697c1f7316079d96158a25cbf710b
rchacon52 <rchacon52@gmail.com>
2024-12-01 10:08:31 -0800

    update

--- terraformk8/main.tf
+++ terraformk8/main.tf
@@ -91,4 +91,21 @@ resource "aws_iam_role_policy_attachment" "additional" {
   role       = each.value.iam_role_name
 }
 
-
+resource "helm_release" "ingress" {
+  name       = "ingress"
+  chart      = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  version    = "1.4.6"
+  set {
+    name  = "autoDiscoverAwsRegion"
+    value = "true"
+  }
+  set {
+    name  = "autoDiscoverAwsVpcID"
+    value = "true"
+  }
+  set {
+    name  = "clusterName"
+    value = var.cluster_name
+  }
+}
