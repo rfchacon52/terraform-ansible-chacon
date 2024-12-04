20b991811f742b5b00e4ea1b0307266fcc854cef
rchacon52 <rchacon52@gmail.com>
2024-12-04 08:16:01 -0800

    update

--- terraformk8/main.tf
+++ terraformk8/main.tf
@@ -12,11 +12,11 @@ provider "aws" {
 }
 #---------------------------------
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 #-------------------------------
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 #-------------------------------
 data "aws_availability_zones" "available" {
