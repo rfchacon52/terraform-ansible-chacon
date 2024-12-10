6ac6bce86b526db4e55d4c46496d78fe55d22099
rchacon52 <rchacon52@gmail.com>
2024-12-09 19:09:52 -0800

    update

--- terraformk8/eks.tf
+++ terraformk8/eks.tf
@@ -9,12 +9,21 @@ module "ek" {
   cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
 
- cluster_addons = {
-    coredns                = {}
-    eks-pod-identity-agent = {}
-    kube-proxy             = {}
-    vpc-cni                = {}
+cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+    aws-ebs-csi-driver = {
+      most_recent = true
+    }
   }
+
 
   subnet_ids =  module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
@@ -29,13 +38,10 @@ eks_managed_node_groups = {
       # This value is ignored after the initial creation
       # https://github.com/bryantbiggs/eks-desired-size-hack
       desired_size = 2
-
+# Needed by the aws-ebs-csi-driver
+      iam_role_additional_policies = {
+        AmazonEBSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
     }
+   }
   }
-}  
-data "aws_eks_cluster" "cluster" {
-  name = module.ek.cluster_name
-}
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.ek.cluster_name
 }
