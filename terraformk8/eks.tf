b11038bc8e67f12e2d1bb0a8fe84badf4a817911
rchacon52 <rchacon52@gmail.com>
2024-12-09 20:40:41 -0800

    update

--- terraformk8/eks.tf
+++ terraformk8/eks.tf
@@ -33,11 +33,11 @@ eks_managed_node_groups = {
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
       instance_types = ["t2.small"]
       ami_type       = "AL2_x86_64"
-      min_size = 2
+      min_size = 1
       max_size = 5
       # This value is ignored after the initial creation
       # https://github.com/bryantbiggs/eks-desired-size-hack
-      desired_size = 2
+      desired_size = 1 
 # Needed by the aws-ebs-csi-driver
       iam_role_additional_policies = {
         AmazonEBSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
