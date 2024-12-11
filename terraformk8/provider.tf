add86faeafd18f0d76abfbc71a57292f0b1eea43
rchacon52 <rchacon52@gmail.com>
2024-12-10 18:37:33 -0800

    up

--- terraformk8/provider.tf
+++ terraformk8/provider.tf
@@ -24,11 +24,11 @@ terraform {
 
 #-----------------------------
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 #---------------------------
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 #------------------------------
 provider "kubernetes" {
