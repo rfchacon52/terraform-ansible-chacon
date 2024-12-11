e22c8d5930779bcf16f9d0133d7d2d7048d6bcd4
rchacon52 <rchacon52@gmail.com>
2024-12-10 18:33:09 -0800

    up

--- terraformk8/provider.tf
+++ terraformk8/provider.tf
@@ -21,6 +21,15 @@ terraform {
     }
   }
 }
+
+#-----------------------------
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+#---------------------------
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
 #------------------------------
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
@@ -34,7 +43,7 @@ provider "helm" {
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
       command     = "aws"
     }
   }
