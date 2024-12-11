45a315c8a1ce2a5e578810af21f460ca0f2758a6
rchacon52 <rchacon52@gmail.com>
2024-12-10 18:59:21 -0800

    up

--- terraformk8/eks.tf
+++ terraformk8/eks.tf
@@ -1,42 +1,3 @@
-module "eks_blueprints_addons" {
-  source = "aws-ia/eks-blueprints-addons/aws"
-  version = "1.19.0" #ensure to update this to the latest/desired version
-
-  cluster_name      = module.eks.cluster_name
-  cluster_endpoint  = module.eks.cluster_endpoint
-  cluster_version   = module.eks.cluster_version
-  oidc_provider_arn = module.eks.oidc_provider_arn
-
-  eks_addons = {
-    aws-ebs-csi-driver = {
-      most_recent = true
-    }
-    coredns = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-  }
-
-  enable_aws_load_balancer_controller    = true
-  enable_cluster_proportional_autoscaler = true
-  enable_karpenter                       = true
-  #enable_kube_prometheus_stack           = true
-  enable_metrics_server                  = true
-  enable_external_dns                    = true
-  enable_cert_manager                    = true
-  cert_manager_route53_hosted_zone_arns  = ["arn:aws:route53:::hostedzone/XXXXXXXXXXXXX"]
-
-  tags = {
-    Environment = "dev"
-  }
-}
-
-
 #----------------------------
 # EKS 
 #---------------------------
@@ -47,6 +8,15 @@ module "eks" {
   cluster_version = "1.31"
   cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
+
+bootstrap_self_managed_addons = false
+  cluster_addons = {
+    vpc-cni                = {} 
+    coredns                = {}
+    eks-pod-identity-agent = {}
+    kube-proxy             = {}
+    aws-ebs-csi-driver     = {} 
+  }
 
   vpc_id      = module.vpc.vpc_id
   subnet_ids  =  module.vpc.private_subnets
