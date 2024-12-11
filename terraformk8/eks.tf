263768befa3d65932125468f8bf3debc7d647d16
rchacon52 <rchacon52@gmail.com>
2024-12-11 12:31:13 -0800

    iup

--- terraformk8/eks.tf
+++ terraformk8/eks.tf
@@ -4,7 +4,7 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.31"
-  cluster_name    = var,cluster_name"
+  cluster_name    = var.cluster_name
   cluster_version = "1.31"
   cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
