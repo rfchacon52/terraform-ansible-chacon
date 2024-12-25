e259832f0df8b5957c0988b5cc641bf7044ab080
rchacon52 <rchacon52@gmail.com>
2024-12-25 13:55:26 -0800

    update

--- terraformk8/vpc.tf
+++ terraformk8/vpc.tf
@@ -11,12 +11,12 @@ provider "aws" {
 }
 #--------------------------
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
 #--------------------------
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
 #--------------------------
