50063b56863c7ee1f77bdc94d3042ff753627b9e
rchacon52 <rchacon52@gmail.com>
2024-12-25 16:14:00 -0800

    update

--- terraformk8/vpc.tf
+++ terraformk8/vpc.tf
@@ -11,27 +11,11 @@ provider "aws" {
 }
 
 
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
-}
-
 #--------------------------
 data "aws_availability_zones" "available" {
   state = "available"
 }
 
-#--------------------------
-module "eks-kubeconfig" {
-  source  = "hyperbadger/eks-kubeconfig/aws"
-  version = "1.0.0"
-
-  depends_on = [module.eks]
-  cluster_id = module.eks.cluster_id
-}
 
 #--------------------------
 # vpc module for eks
