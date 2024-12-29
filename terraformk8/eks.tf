6c32c286dafc56ca175c9fb65aab707d107ed54c
rchacon52 <rchacon52@gmail.com>
2024-12-29 02:07:52 -0800

    update

--- terraformk8/eks.tf
+++ terraformk8/eks.tf
@@ -8,7 +8,6 @@ module "eks" {
   cluster_name    = var.cluster_name
   cluster_version = "1.31"
   cluster_endpoint_public_access           = true
-  enable_cluster_creator_admin_permissions = true
 
   vpc_id      = module.vpc.vpc_id
   subnet_ids  =  module.vpc.private_subnets
