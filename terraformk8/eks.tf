d72eeec3bcfbbcea0a1a1f692fd013907032645e
rchacon52 <rchacon52@gmail.com>
2024-12-31 19:12:45 -0800

    update

--- terraformk8/eks.tf
+++ terraformk8/eks.tf
@@ -73,14 +73,6 @@ module "eks" {
   }
 }
 
-# https://github.com/terraform-aws-modules/terraform-aws-eks/issues/2009
-data "aws_eks_cluster" "default" {
-  name = module.eks.cluster_name
-}
-
-data "aws_eks_cluster_auth" "default" {
-  name = module.eks.cluster_name
-}
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.default.endpoint
