4c4001d1908d2c09c1e1477d2179f386b729e6d5
rchacon52 <rchacon52@gmail.com>
2025-01-01 04:17:47 -0800

    update

--- terraformk8/data.tf
+++
@@ -1,8 +0,0 @@
-# https://github.com/terraform-aws-modules/terraform-aws-eks/issues/2009
-data "aws_eks_cluster" "default" {
-  name = module.eks.cluster_name
-}
-
-data "aws_eks_cluster_auth" "default" {
-  name = module.eks.cluster_name
-}
