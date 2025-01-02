1251046b57da492f83b0ab2298c6c09e11f31131
rchacon52 <rchacon52@gmail.com>
2025-01-01 16:51:17 -0800

    update

--- terraformk8/provider.tf
+++ terraformk8/provider.tf
@@ -39,5 +39,11 @@ data "aws_eks_cluster_auth" "default" {
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.default.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.default.token
+  # token                  = data.aws_eks_cluster_auth.default.token
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]
+    command     = "aws"
+  }
 }
