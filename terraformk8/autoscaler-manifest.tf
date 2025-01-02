9dec65be465a304b1b00d82d2bce5342732a34c9
rchacon52 <rchacon52@gmail.com>
2025-01-02 14:53:55 -0800

    update

--- terraformk8/autoscaler-manifest.tf
+++ terraformk8/autoscaler-manifest.tf
@@ -3,16 +3,6 @@ provider "kubectl" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   load_config_file       = false
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws-iam-authenticator"
-    args = [
-      "token",
-      "-i",
-      module.eks.cluster_id,
-    ]
-  }
 }
 
 
