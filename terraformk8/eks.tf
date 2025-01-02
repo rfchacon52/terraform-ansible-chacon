7aaadf6f0009a9b26e2e9ad3f6437089071735cf
rchacon52 <rchacon52@gmail.com>
2025-01-02 01:07:50 -0800

    update

--- terraformk8/eks.tf
+++ terraformk8/eks.tf
@@ -54,7 +54,7 @@ data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_name
 }
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
 provider "kubernetes" {
