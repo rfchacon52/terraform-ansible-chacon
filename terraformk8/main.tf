91ef335ad1747679ddaa471f17b2f8a45db642e4
rchacon52 <rchacon52@gmail.com>
2025-01-03 14:43:01 -0800

    update

--- terraformk8/main.tf
+++ terraformk8/main.tf
@@ -87,7 +87,6 @@ module "eks" {
   cluster_version                = "1.31"
   cluster_endpoint_public_access = true
   cluster_endpoint_private_access = true
-  cluster_endpoint_public_access = true
   enable_cluster_creator_admin_permissions = true
   enable_irsa = true
 
