67916452189c349192ebfcf03f1f4160fa19119e
rchacon52 <rchacon52@gmail.com>
2025-01-03 08:04:56 -0800

    update

--- terraformk8/versions.tf
+++ terraformk8/versions.tf
@@ -1,18 +1,18 @@
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 1.5.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.47"
+      version = ">= 5.75.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.9"
+      version = ">= 2.14.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.20"
+      version = ">= 2.35.1"
     }
     random = {
       source  = "hashicorp/random"
@@ -24,7 +24,7 @@ terraform {
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
-      version = ">= 1.14"
+      version = "1.18.0"
     }
   }
   
