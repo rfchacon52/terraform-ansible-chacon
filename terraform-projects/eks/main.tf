01317559826438d36aa9ce4d34035cf73f90618f
rchacon52 <rchacon52@gmail.com>
2025-01-04 21:59:39 -0800

    update

--- terraform-projects/eks/main.tf
+++ terraform-projects/eks/main.tf
@@ -1,7 +1,11 @@
 ################################################################################
-# VPC Module
+# Root module calls supporting modules. All variables and providers defined in
+# variables.tf amd provider.tf  
 ################################################################################
 
+################################################################################
+# VPC Module
+################################################################################
 module "vpc" {
   source = "./modules/vpc"
 }
@@ -9,27 +13,17 @@ module "vpc" {
 ################################################################################
 # EKS Cluster Module
 ################################################################################
-
 module "eks" {
   source = "./modules/eks-cluster"
-
-  rolearn     = var.rolearn
-
-  vpc_id          = module.vpc.vpc_id
-  private_subnets = module.vpc.private_subnets
 }
 
 ################################################################################
 # AWS ALB Controller
 ################################################################################
+module "aws_alb_controller" {
+  source = "./modules/aws-alb-controller"
 
-#module "aws_alb_controller" {
-#  source = "./modules/aws-alb-controller"
-
-#  env_name     = var.env_name
-#  cluster_name = var.cluster_name
-
-#  vpc_id            = module.vpc.vpc_id
-#  oidc_provider_arn = module.eks.oidc_provider_arn
-#}
+  vpc_id            = module.vpc.vpc_id
+  oidc_provider_arn = module.eks.oidc_provider_arn
+}
 
