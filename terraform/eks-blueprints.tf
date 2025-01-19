00038e4b35a5e8274498ec7e503fab40f18405a9
rchacon52 <rchacon52@gmail.com>
2025-01-19 00:13:53 -0800

    up

--- terraform/eks-blueprints.tf
+++ terraform/eks-blueprints.tf
@@ -98,33 +98,3 @@ module "eks" {
   }
 }
 
-#--------------------------------
-
-
-data "aws_eks_cluster" "this" {
-  name = var.cluster_name 
- depends_on = [module.eks]
-}
-
-data "aws_eks_cluster_auth" "this" {
-  name = var.cluster_name 
- depends_on = [module.eks]
-}
-
-provider "kubernetes" {
-  host = data.aws_eks_cluster.this.endpoint
-
-  token                  = data.aws_eks_cluster_auth.this.token
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority.0.data)
-
-}
-
-provider "helm" {
-  kubernetes {
-    host = data.aws_eks_cluster.this.endpoint
-
-    token                  = data.aws_eks_cluster_auth.this.token
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority.0.data)
-  }
-}
-
