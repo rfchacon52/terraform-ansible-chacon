518a9d12c1d159247d0f9eff8ff3e837037e6274
rchacon52 <rchacon52@gmail.com>
2025-02-03 00:46:38 -0800

    up

--- terraform-asg/ec2.tf
+++ terraform-asg/ec2.tf
@@ -5,7 +5,6 @@ module "ec2_instance" {
 
   instance_type          = "t2.micro"
   key_name               = "deployer.key"
-  monitoring             = true
   vpc_security_group_ids = [aws_security_group.terra_SG.id] 
   subnet_id              = module.vpc.public_subnets[0]
 
@@ -14,11 +13,20 @@ module "ec2_instance" {
     Environment = "dev"
   }
 
-  user_data = <<-EOF
-  #!/bin/bash
-  echo "*** Installing apache2"
-  sudo apt update -y
-  sudo apt install apache2 -y
-  echo "*** Completed Installing apache2"
-  EOF
+}
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "ec2-2"
+
+  instance_type          = "t2.micro"
+  key_name               = "deployer.key"
+  vpc_security_group_ids = [aws_security_group.terra_SG.id] 
+  subnet_id              = module.vpc.public_subnets[1]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+
 }
