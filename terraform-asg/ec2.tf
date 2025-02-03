f47c39064b9f4842f22fca732a5896be5c38f7c3
rchacon52 <rchacon52@gmail.com>
2025-02-03 01:16:24 -0800

    up

--- terraform-asg/ec2.tf
+++ terraform-asg/ec2.tf
@@ -1,16 +1,12 @@
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "ec2-1"
-
-  instance_type          = "t2.micro"
+resource "aws_instance" "by_chaining" {
+  for_each = module.vpc.public_subnets 
+  instance_type = "t2.micro"
   key_name               = "deployer.key"
-  vpc_security_group_ids = [aws_security_group.terra_SG.id] 
-  subnet_id              = module.vpc.public_subnets[0]
+  vpc_security_group_ids = [aws_security_group.terra_SG.id]
+  subnet_id = each.value.id
 
   tags = {
-    Terraform   = "true"
-    Environment = "dev"
+    Name = "${each.value.id} instance"
   }
-
 }
