372d1846832a8610f68120d6475b3592eac2887b
rchacon52 <rchacon52@gmail.com>
2025-02-03 00:53:57 -0800

    up

--- terraform-asg/ec2.tf
+++ terraform-asg/ec2.tf
@@ -14,19 +14,3 @@ module "ec2_instance" {
   }
 
 }
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-
-  name = "ec2-2"
-
-  instance_type          = "t2.micro"
-  key_name               = "deployer.key"
-  vpc_security_group_ids = [aws_security_group.terra_SG.id] 
-  subnet_id              = module.vpc.public_subnets[1]
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
-
-}
