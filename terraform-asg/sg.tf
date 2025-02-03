a34e6d28cb36b4b8b4e83a12697389316c3761a5
rchacon52 <rchacon52@gmail.com>
2025-02-02 18:10:07 -0800

    update

--- terraform-asg/sg.tf
+++ terraform-asg/sg.tf
@@ -1,58 +1,24 @@
 # create security group to allow ingoing ports
+
 resource "aws_security_group" "terra_SG" {
   name        = "sec_group"
   description = "security group for the EC2 instance"
   vpc_id   = module.vpc.vpc_id
-  ingress = [
-    {
-      description      = "https traffic"
-      from_port        = 443
-      to_port          = 443
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0", module.vpc.vpc_id.cidr]
-      ipv6_cidr_blocks  = []
-      prefix_list_ids   = []
-      security_groups   = []
-      self              = false
-    },
-    {
-      description      = "http traffic"
-      from_port        = 80
-      to_port          = 80
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0", module.vpc.vpc_id.cidr]
-      ipv6_cidr_blocks  = []
-      prefix_list_ids   = []
-      security_groups   = []
-      self              = false
-    },
-    {
-      description      = "ssh"
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0", module.vpc.vpc_id.cidr]
-      ipv6_cidr_blocks  = []
-      prefix_list_ids   = []
-      security_groups   = []
-      self              = false
-    }
-  ]
-  egress = [
-    {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      description      = "Outbound traffic rule"
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
-    }
-  ]
-  tags = {
-    name = "allow_web"
+
+tags = {
+    Name = "terra_SG"
   }
 }
+
+
+resource "aws_vpc_security_group_ingress_rule" "terra_SG" {
+  security_group_id = aws_security_group.terra_SG.id
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+}
+
+
+
+
 
