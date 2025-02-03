e576fa6ef90f1d0aef4f851621c206e3605385a7
rchacon52 <rchacon52@gmail.com>
2025-02-02 17:51:26 -0800

    update

--- terraform-asg/sg.tf
+++ terraform-asg/sg.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "terra_SG" {
       from_port        = 443
       to_port          = 443
       protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0", aws_vpc.terra_vpc.cidr_block]
+      cidr_blocks      = ["0.0.0.0/0", module.vpc.vpc_id.cidr]
       ipv6_cidr_blocks  = []
       prefix_list_ids   = []
       security_groups   = []
@@ -20,7 +20,7 @@ resource "aws_security_group" "terra_SG" {
       from_port        = 80
       to_port          = 80
       protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0", aws_vpc.terra_vpc.cidr_block]
+      cidr_blocks      = ["0.0.0.0/0", module.vpc.vpc_id.cidr]
       ipv6_cidr_blocks  = []
       prefix_list_ids   = []
       security_groups   = []
@@ -31,7 +31,7 @@ resource "aws_security_group" "terra_SG" {
       from_port        = 22
       to_port          = 22
       protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0", aws_vpc.terra_vpc.cidr_block]
+      cidr_blocks      = ["0.0.0.0/0", module.vpc.vpc_id.cidr]
       ipv6_cidr_blocks  = []
       prefix_list_ids   = []
       security_groups   = []
