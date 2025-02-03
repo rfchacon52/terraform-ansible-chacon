243ef3a1150b2be01fb244375fe536d3a976c1ba
rchacon52 <rchacon52@gmail.com>
2025-02-02 18:15:15 -0800

    update

--- terraform-asg/sg.tf
+++ terraform-asg/sg.tf
@@ -13,6 +13,7 @@ tags = {
 
 resource "aws_vpc_security_group_ingress_rule" "terra_SG" {
   security_group_id = aws_security_group.terra_SG.id
+  cidr_ipv4 = "10.0.0.0/16"
   from_port   = 80
   ip_protocol = "tcp"
   to_port     = 80
