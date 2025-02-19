cee84ced9b789b6bcc7325a1e8d20452b3d0a0d2
rchacon52 <rchacon52@gmail.com>
2025-02-18 20:13:12 -0800

    up

--- terraform-asg/ec2.tf
+++ terraform-asg/ec2.tf
@@ -2,7 +2,7 @@ module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
   name = "Bastion"
-  image_id  = "ami-0c7af5fe939f2677f"
+  # image_id  = "ami-0c7af5fe939f2677f"
   instance_type          = "t2.micro"
   key_name               = "deployer.key"
   vpc_security_group_ids = [aws_security_group.terra_SG.id] 
