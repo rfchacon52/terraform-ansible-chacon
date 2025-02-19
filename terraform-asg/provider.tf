ccd278c830de3edfcaedd8bfdba4f4248d4b2631
rchacon52 <rchacon52@gmail.com>
2025-02-19 12:54:17 -0800

    update

--- terraform-asg/provider.tf
+++ terraform-asg/provider.tf
@@ -13,7 +13,7 @@ terraform {
   backend "s3" {
     bucket         = "chacon-backend"
     key            = "terraform/state"
-    region         = "us-west-2"
+    region         = "us-east-1"
     dynamodb_table = "terraform-lock-table"
   }
 }
