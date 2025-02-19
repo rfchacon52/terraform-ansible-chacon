ba5f92836ea231d64deab66af3033978f9867b67
rchacon52 <rchacon52@gmail.com>
2025-02-19 10:02:38 -0800

    update

--- terraform-asg/ec2.tf
+++ terraform-asg/ec2.tf
@@ -6,7 +6,7 @@ module "ec2_instance" {
   instance_type          = "t2.micro"
   key_name               = "deployer.key"
   monitoring             = true
-  vpc_security_group_ids = [aws_vpc_security_group_ingress_rule.terra-SG-22]
+  vpc_security_group_ids = [aws_security_group.ec2-sg.id]
   subnet_id              = module.vpc.public_subnets[0]
   tags = {
     Name   = "Bastion"
