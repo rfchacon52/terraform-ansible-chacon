5472f797962d90160a3632ef08b95155fe77ca95
rchacon52 <rchacon52@gmail.com>
2025-02-20 02:34:45 -0800

    up

--- terraform-asg/user-data.sh
+++ terraform-asg/user-data.sh
@@ -2,16 +2,6 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-sudo echo "[nginx-stable]" > /tmp/nginx.repo
-sudo echo "name=nginx stable repo" >> /tmp/nginx.repo
-sudo echo "baseurl=http://nginx.org/packages/centos/$releasever/$basearch/" >> /tmp/nginx.repo
-sudo echo "gpgcheck=1" >>  /tmp/nginx.repo
-sudo echo "enabled=1" >>  /tmp/nginx.repo   
-sudo echo "gpgkey=https://nginx.org/keys/nginx_signing.key" >> /tmp/nginx.repo
-sudo echo "module_hotfixes=true" >> /tmp/nginx.repo
-sudo cp /tmp/nginx.repo  /etc/yum.repos.d/
-sudo yum-config-manager --enable nginx-stable
-# sudo yum update -y
 sudo yum install -y nginx
 sudo systemctl enable nginx
 sudo systemctl start nginx
