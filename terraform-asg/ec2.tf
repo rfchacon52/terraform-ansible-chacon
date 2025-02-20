2901ce26029172da5b1bbbd6b4215e7cef2e764c
rchacon52 <rchacon52@gmail.com>
2025-02-19 16:59:24 -0800

    update

--- terraform-asg/ec2.tf
+++ terraform-asg/ec2.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "Bastion" {
   key_name   = "deployer.key"
   vpc_security_group_ids      = [aws_security_group.ec2-sg.id, aws_vpc_security_group_ingress_rule.terra-SG-22.id]
   associate_public_ip_address = true
+  user_data = file("user-data.sh")
  root_block_device {
    volume_type           = "gp2"
    volume_size           = "20"
@@ -15,25 +16,4 @@ resource "aws_instance" "Bastion" {
     Name = "Bastion"
   }
 
-user_data = << EOF
-  #!/bin/bash
-  sudo yum update -y
-  sudo yum install -y epel-release  
-  sudo yum install -y nginx
-  sudo systemctl enable nginx
-  sudo systemctl start nginx
-  sudo echo "<!DOCTYPE html>" > /tmp/index.html
-  sudo echo "<html> >> /tmp/index.html
-  sudo echo "<body style="background-color:red;">" >> /tmp/index.html
-  sudo echo "<h1>This is host my_hostname </h1>" >> /tmp/index.html
-  sudo echo "</body>" >> /tmp/index.html
-  sudo echo "</html>" >> /tmp/index.html
-  host_name=$(/usr/bin/hostname)
-  sed -i -e s/my_hostname/$host_name/g /tmp/index.html
-  sudo systemctl stop nginx
-  sleep 3
-  sudo rm -f /usr/share/testpage/index.html
-  sudo cp /tmp/index.html /usr/share/testpage
-  sudo systemctl start nginx
-EOF
 }
--- terraform-asg/user-data.sh
+++ terraform-asg/user-data.sh
@@ -2,23 +2,21 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-sudo yum update -y
-sudo yum install -y epel-release  
-sudo yum install -y nginx
-sudo systemctl enable nginx
-sudo systemctl start nginx
-
-sudo echo "<!DOCTYPE html>" > /tmp/index.html
-sudo echo "<html> >> /tmp/index.html
-sudo echo "<body style="background-color:red;">" >> /tmp/index.html
-sudo echo "<h1>This is host my_hostname </h1>" >> /tmp/index.html
-sudo echo "</body>" >> /tmp/index.html
-sudo echo "</html>" >> /tmp/index.html
-
-host_name=$(/usr/bin/hostname)
-sed -i -e s/my_hostname/$host_name/g /tmp/index.html
-sudo systemctl stop nginx
-sleep 3
-sudo rm -f /usr/share/testpage/index.html
-sudo cp /tmp/index.html /usr/share/testpage
-sudo systemctl start nginx
+  sudo yum update -y
+  sudo yum install -y epel-release  
+  sudo yum install -y nginx
+  sudo systemctl enable nginx
+  sudo systemctl start nginx
+  sudo echo "<!DOCTYPE html>" > /tmp/index.html
+  sudo echo "<html> >> /tmp/index.html
+  sudo echo "<body style="background-color:red;">" >> /tmp/index.html
+  sudo echo "<h1>This is host my_hostname </h1>" >> /tmp/index.html
+  sudo echo "</body>" >> /tmp/index.html
+  sudo echo "</html>" >> /tmp/index.html
+  host_name=$(/usr/bin/hostname)
+  sed -i -e s/my_hostname/$host_name/g /tmp/index.html
+  sudo systemctl stop nginx
+  sleep 3
+  sudo rm -f /usr/share/testpage/index.html
+  sudo cp /tmp/index.html /usr/share/testpage
+  sudo systemctl start nginx
