bc2e1f8011670ba43ca9effc5783d2266fe50a53
rchacon52 <rchacon52@gmail.com>
2025-02-20 13:04:59 -0800

    update

--- terraform-asg/main.tf
+++ terraform-asg/main.tf
@@ -92,4 +92,35 @@ resource "aws_autoscaling_group" "ec2-sg" {
   }
     health_check_type = "EC2"
 
+
+##############################
+# Auto Scaling Policy 
+##############################
+resource "aws_autoscaling_policy" "scale_down" {
+  name                   = "terramino_scale_down"
+  autoscaling_group_name = aws_autoscaling_group.ec2-sg.name
+  adjustment_type        = "ChangeInCapacity"
+  scaling_adjustment     = -1
+  cooldown               = 120
+}
+
+##############################
+# CloudWatch Metric Alarm 
+##############################
+resource "aws_cloudwatch_metric_alarm" "scale_down" {
+  alarm_description   = "Monitors CPU utilization for Terramino ASG"
+  alarm_actions       = [aws_autoscaling_policy.scale_down.arn]
+  alarm_name          = "terramino_scale_down"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  namespace           = "AWS/EC2"
+  metric_name         = "CPUUtilization"
+  threshold           = "10"
+  evaluation_periods  = "2"
+  period              = "120"
+  statistic           = "Average"
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.ec2-sg.name
+  }
+}
 }
