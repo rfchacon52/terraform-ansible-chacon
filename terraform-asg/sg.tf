6dfcc4b17fc8f407354aac1fb6ce0091548c3674
rchacon52 <rchacon52@gmail.com>
2025-02-19 17:09:05 -0800

    update

--- terraform-asg/sg.tf
+++ terraform-asg/sg.tf
@@ -64,14 +64,18 @@ resource "aws_security_group" "ec2-sg" {
   name = "ec2-sg1"
   description = "Security Group for ec2"
   vpc_id   = module.vpc.vpc_id
-
   ingress {
     from_port = 80 
     to_port   = 80
-    protocol  = "http"
+    protocol  = "tcp"
     cidr_blocks = ["10.0.0.0/16"]
   }
-
+  ingress  {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
  egress {
     from_port = 0
     to_port   = 0
