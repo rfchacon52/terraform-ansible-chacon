2b5676135345c33f00495f31d0a612b5def0b843
rchacon52 <rchacon52@gmail.com>
2025-03-04 10:57:30 -0800

    update

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,7 @@
+###################################################
+# State file 
+###################################################
+
 terraform {
 
 required_version = ">= 1.5.0"
@@ -22,6 +26,10 @@ required_version = ">= 1.5.0"
 
   }
 
+###################################################
+# State file 
+###################################################
+
   backend "s3" {
     bucket         = "chacon-backend"
     key            = "terraform/state"
@@ -43,5 +51,31 @@ variable "cluster_name" {
 }
 variable "cluster_version" {
   default = "1.31"
+}
+
+###################################################
+# Configure providers
+###################################################
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.this.token
+  }
+}
+
+provider "kubectl" {
+  apply_retry_count      = 10
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  load_config_file       = false
+  token                  = data.aws_eks_cluster_auth.this.token
 }
 
