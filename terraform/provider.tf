13e3fa42083a579596110d7c2297dcc825e52593
rchacon52 <rchacon52@gmail.com>
2025-03-05 22:56:31 -0800

    update

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -59,7 +59,7 @@ provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.this.token
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
 
@@ -68,6 +68,6 @@ provider "kubectl" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   load_config_file       = false
-  token                  = data.aws_eks_cluster_auth.this.token
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
