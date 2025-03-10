465238486a5cee3b6823fe71b5433883e7cede1d
rchacon52 <rchacon52@gmail.com>
2025-03-10 13:10:32 -0700

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,6 +53,12 @@ module "eks" {
   }
 }
 }
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
 ################################################################################
 # EKS Blue Prints Addons 
 ################################################################################
