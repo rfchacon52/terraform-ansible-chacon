db53db70a004beeff6b60672524387bea1865afe
rchacon52 <rchacon52@gmail.com>
2025-03-10 13:44:48 -0700

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,6 +53,18 @@ module "eks" {
       min_size     = 1
   }
 }
+
+node_security_group_additional_rules = {
+    ingress_allow_access_from_control_plane = {
+      type                          = "ingress"
+      protocol                      = "tcp"
+      from_port                     = 9443
+      to_port                       = 9443
+      source_cluster_security_group = true
+      description                   = "Allow access from control plane to webhook port of AWS load balancer controller"
+    }
+  }
+
 }
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
