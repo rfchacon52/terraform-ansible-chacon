e34875769022d9af962ca9cf52517b4ce16323c6
rchacon52 <rchacon52@gmail.com>
2025-03-10 12:58:19 -0700

    update

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -7,13 +7,6 @@ data "aws_ecrpublic_authorization_token" "token" {
 
 data "aws_availability_zones" "available" {}
 
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
-}
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
