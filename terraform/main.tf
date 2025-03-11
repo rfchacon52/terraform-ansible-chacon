a6e7abcf1f7350e7c1be004c490a37a793253fcd
rchacon52 <rchacon52@gmail.com>
2025-03-10 19:57:43 -0700

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -91,8 +91,8 @@ module "eks_blueprints_addons" {
   }
 
   # enable_cluster_proportional_autoscaler = true
-  # enable_kube_prometheus_stack           = true
- # enable_metrics_server                  = true
+  enable_kube_prometheus_stack           = true
+  enable_metrics_server                  = true
   #enable_external_dns                    = true
   #enable_cert_manager                    = true
  # cert_manager_route53_hosted_zone_arns  = ["arn:aws:route53:::hostedzone/XXXXXXXXXXXXX"]
