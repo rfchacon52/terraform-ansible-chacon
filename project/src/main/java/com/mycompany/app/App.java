106d70efb38a308cdd10e14923a267546b3f84b1
rchacon52 <rchacon52@gmail.com>
2025-03-31 13:15:10 -0700

    update

--- project/src/main/java/com/mycompany/app/App.java
+++ project/src/main/java/com/mycompany/app/App.java
@@ -4,10 +4,22 @@ package com.mycompany.app;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("Looping...");
+            // Optionally, add a small delay to prevent excessive CPU usage.
+            try {
+                Thread.sleep(1000); // Sleep for 1 second
+            } catch (InterruptedException e) {
+                // If interrupted, the loop will continue, or you can handle it differently.
+                System.err.println("Loop interrupted");
+            }
+
+            // you can also add some other code here that you want to run every loop.
+        }
     }
 }
+
+
