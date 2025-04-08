6d64b1780beb321cdf87ae173a8af57353ba6211
rchacon52 <rchacon52@gmail.com>
2025-04-07 23:26:28 -0700

    update

--- sftp/s3.tf
+++ sftp/s3.tf
@@ -3,24 +3,24 @@
 # S3 Buckets
 # -----------------------------------------------------------------------------
 
-resource "aws_s3_bucket" "sftp-home-bucket"  {
-  bucket = "sftp-home-bucket" 
+resource "aws_s3_bucket" "chacon-sftp-home-bucket"  {
+  bucket = "chacon-sftp-home-bucket" 
   }
 
-resource "aws_s3_bucket_acl" "sftp-home-bucket" {
-  bucket = aws_s3_bucket.sftp-home-bucket.id
+resource "aws_s3_bucket_acl" "chacon-sftp-home-bucket" {
+  bucket = aws_s3_bucket.chacon-sftp-home-bucket.id
   acl    = "private"
 }
 
-resource "aws_s3_bucket_versioning" "versioning-sftp-home-bucket" {
-  bucket = aws_s3_bucket.sftp-home-bucket.id
+resource "aws_s3_bucket_versioning" "versioning-chacon-sftp-home-bucket" {
+  bucket = aws_s3_bucket.chacon-sftp-home-bucket.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
 resource "aws_s3_bucket_public_access_block" "sftp-home-block" {
-  bucket                  = aws_s3_bucket.sftp-home-bucket.id
+  bucket                  = aws_s3_bucket.chacon-sftp-home-bucket.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
