0477dfa0c1739da69ecef3b46829dadc956f5fee
rchacon52 <rchacon52@gmail.com>
2025-04-08 00:52:54 -0700

    update

--- sftp/s3.tf
+++ sftp/s3.tf
@@ -6,10 +6,21 @@
 resource "aws_s3_bucket" "chacon-sftp-home-bucket"  {
   bucket = "chacon-sftp-home-bucket" 
   }
+
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+  bucket = aws_s3_bucket.chacon-sftp-home-bucket.id
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+}
+
+
 resource "aws_s3_bucket_acl" "chacon-sftp-home-bucket" {
   bucket = aws_s3_bucket.chacon-sftp-home-bucket.id
   acl    = "private"
+  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
 }
+
 resource "aws_s3_bucket_versioning" "versioning-chacon-sftp-home-bucket" {
   bucket = aws_s3_bucket.chacon-sftp-home-bucket.id
   versioning_configuration {
