24a44b89cda17cc0483421cc5f586eb4c15bf7e4
rchacon52 <rchacon52@gmail.com>
2025-04-07 23:00:08 -0700

    up

--- sftp/s3.tf
+++ sftp/s3.tf
@@ -3,24 +3,24 @@
 # S3 Buckets
 # -----------------------------------------------------------------------------
 
-resource "aws_s3_bucket" "sftp_home_bucket"  {
-  bucket = "sftp_home_bucket" 
+resource "aws_s3_bucket" "sftp-home-bucket"  {
+  bucket = "sftp-home-bucket" 
   }
 
-resource "aws_s3_bucket_acl" "sftp_home_bucket" {
-  bucket = aws_s3_bucket.sftp_home_bucket.id
+resource "aws_s3_bucket_acl" "sftp-home-bucket" {
+  bucket = aws_s3_bucket.sftp-home-bucket.id
   acl    = "private"
 }
 
-resource "aws_s3_bucket_versioning" "versioning_sftp_home_bucket" {
-  bucket = aws_s3_bucket.sftp_home_bucket.id
+resource "aws_s3_bucket_versioning" "versioning-sftp-home-bucket" {
+  bucket = aws_s3_bucket.sftp-home-bucket.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "sftp_home_block" {
-  bucket                  = aws_s3_bucket.sftp_home_bucket.id
+resource "aws_s3_bucket_public_access_block" "sftp-home-block" {
+  bucket                  = aws_s3_bucket.sftp-home-bucket.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
