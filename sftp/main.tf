b8cf50ee7e5687999ab7785cb5c202dafc1ecba5
rchacon52 <rchacon52@gmail.com>
2025-04-07 23:58:20 -0700

    update

--- sftp/main.tf
+++ sftp/main.tf
@@ -59,7 +59,8 @@ resource "aws_iam_policy" "sftp_user_policy" {
           "s3:DeleteObjectVersion",
           "s3:GetObjectVersion",
           "s3:GetObjectACL",
-          "s3:PutObjectACL"
+          "s3:PutObjectACL",
+          "s3.PutBucketAcl"
         ]
         Resource = [
           "${aws_s3_bucket.chacon-sftp-home-bucket.arn}/sftp-user-home/*"
