e7fe7a1e552fb4c94aac83b698eb3a06b8839a1f
rchacon52 <rchacon52@gmail.com>
2025-04-09 13:20:46 -0700

    update

--- sftp/sftp.tf
+++ sftp/sftp.tf
@@ -96,8 +96,9 @@ resource "aws_transfer_user" "transfer_user" {
   }
 }
 #Generate SSH key 
-resource "aws_transfer_ssh_key" "ssh_key" {
+/* resource "aws_transfer_ssh_key" "ssh_key" {
   server_id = aws_transfer_server.transfer_server.id
   user_name = "sftp-user" 
   body = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDI81EOwYm7z2fiXrEFWeCDU16V2g3MbJFt35DhntyeQEIpuExmYxwdZ1i3rbldDb6Y7zbKlTMj25WwOFCz+kHQlKCtqggGKMxG2qgg+CjG5CPReYA3T8gRAsaGnM+xwlLwjPVY+edKuRzZpFdAPe44Kj3cuwKguVH/MqtvcSfbZBo8BAChm3P2koYXW01kWCIbfy778T0ADzCSGzqC5UwEmhZ6oHN6QXzDDqWSDTWDYgBagGd/8vgDtr9BaDUlFw8YJ9Q21bMIuCzFOef5aac1Vr0copa3zolVvznt86YzAi9LKCACSfVRRzRrS3VOSAS8I0QOynE7VsxlhqT0p2Sn" 
   }
+*/
