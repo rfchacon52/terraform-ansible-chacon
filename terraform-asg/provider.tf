4ee1c2ce0655245c8ea4cb20a5d2d3e785a0e795
rchacon52 <rchacon52@gmail.com>
2025-04-22 12:18:10 -0700

    update

--- terraform-asg/provider.tf
+++ terraform-asg/provider.tf
@@ -1,19 +1,38 @@
-provider "aws" {
-  region = "us-east-1"
-
-  default_tags {
-    tags = {
-      hashicorp-learn = "aws-asg"
-    }
-  }
-}
-
+###################################################
+# State file 
+###################################################
 
 terraform {
-  backend "s3" {
-    bucket         = "chacon-backend2"
-    key            = "terraform/state"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock-table"
+
+required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.94.1"
+    }
+
   }
+
+
+
+###################################################
+# State file 
+###################################################
+
+  backend "s3" {
+    bucket         = "chacon-backend3"
+    key            = "terraform/state"
+    region         = "us-east-2"
+    use_lockfile   = true
+  }
+
 }
+
+
+provider "aws" {
+  region = "us-east-1"
+  alias  = "virginia"
+}
+
+
