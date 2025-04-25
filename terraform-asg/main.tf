1b96c041fad0aedb20889cb488e6ec4ad170303f
rchacon52 <rchacon52@gmail.com>
2025-04-24 23:42:15 -0700

    update

--- terraform-asg/main.tf
+++ terraform-asg/main.tf
@@ -131,8 +131,5 @@ resource "aws_autoscaling_policy" "scale_up_policy" {
   autoscaling_group_name  = aws_autoscaling_group.auto_scaling_group.name
 }
 
-output "alb_dns_name" {
-  value = aws_lb.application_load_balancer.dns_name
-}
 
 
