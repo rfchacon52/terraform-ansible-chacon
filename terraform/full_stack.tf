b139402bbba0737bc3a99132f9709443922e691a
rfchacon52 <rfchacon52@gmail.com>
2025-05-20 22:21:46 -0700

    Bob

--- terraform/full_stack.tf
+++ terraform/full_stack.tf
@@ -71,7 +71,7 @@ module "eks" {
   version = "20.36.0" # Use the latest version
 
   cluster_name    = local.name 
-  cluster_version = "1.32" # Specify your desired Kubernetes version
+  cluster_version = "1.31" # Specify your desired Kubernetes version
   vpc_id          = module.vpc.vpc_id
   subnet_ids      = module.vpc.private_subnets
 
@@ -80,10 +80,11 @@ module "eks" {
   cluster_endpoint_public_access  = true
   create_cluster_security_group   = true
 
+
   # Managed node group configuration
   eks_managed_node_groups = {
-    default = {
-      name = "default"
+    EKS_Blue = {
+      name = "EKS-Blue"
       ami_type       =  var.ami_type
       instance_types = ["t2.small"]
       desired_capacity = 2
@@ -136,4 +137,88 @@ enable_metrics_server                  = true
 enable_cert_manager                    = true
   # cert_manager_route53_hosted_zone_arns  = ["arn:aws:route53:::hostedzone/XXXXXXXXXXXXX"]
 
+}
+# Example IAM role for EBS CSI Driver (if you use it)
+resource "aws_iam_role" "ebs-csi-driver-role" {
+  name = "ebs-csi-driver-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "eks.amazonaws.com"
+        },
+        Condition = {
+          StringEquals = {
+            "sts:OIDCProvider": "oidc.eks.us-east-1.amazonaws.com/id/767397937300" # Replace
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "ebs_csi_driver_policy_attachment" {
+  roles      = [aws_iam_role.ebs-csi-driver-role.name] # <-- THIS IS THE MISSING PART
+   name  = aws_iam_role.ebs-csi-driver-role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+
+
+
+# Example IAM role for EKS Node Group
+resource "aws_iam_role" "eks_node_group_role" {
+  name = "eks-node-group-role"
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+# Attach policies to the node group role
+resource "aws_iam_role_policy_attachment" "eks_node_group_policy_attachment" {
+  role = aws_iam_role.eks_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group_policy_attachment_2" {
+  role = aws_iam_role.eks_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group_policy_attachment_3" {
+  role = aws_iam_role.eks_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+###########################################
+# Apply EKS Cluster Roles 
+############################################
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+# Attach the necessary policy to the cluster role
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy_attachment" {
+  role  = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
