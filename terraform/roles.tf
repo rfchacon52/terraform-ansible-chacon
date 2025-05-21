1791a08ec5171f5be7760a5e3b7cf35393f807ba
rfchacon52 <rfchacon52@gmail.com>
2025-05-20 20:50:32 -0700

    tom

--- terraform/roles.tf
+++ terraform/roles.tf
@@ -24,31 +24,38 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy_attachment" {
 ###########################################
 # Apply EBS CSI Driver  
 ############################################
+
 resource "aws_iam_role" "ebs-csi-driver-role" {
   name = "ebs-csi-driver-role"
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [{
-        Action = "sts:AssumeRole",
+    Statement = [
+      {
         Effect = "Allow",
         Principal = {
-          Service = "eks.amazonaws.com"
+          # CORRECTED: Use Federate to specify the OIDC provider
+          # You should get the OIDC provider URL from your EKS cluster output
+          # e.g., module.eks.oidc_provider_arn
+          Federated = "arn:aws:iam::ACCOUNT_ID:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/767397937300"
         },
+        Action = "sts:AssumeRoleWithWebIdentity", # CORRECTED: This action is for web identity federation
         Condition = {
           StringEquals = {
-            "sts:OIDCProvider": "oidc.eks.us-east-1.amazonaws.com/id/767397937300" # Replace
+            # This condition ensures only the specific K8s service account can assume the role
+            "oidc.eks.us-east-1.amazonaws.com/id/767397937300:sub": "system:serviceaccount:kube-system:ebs-csi-controller-sa" # CORRECTED: Specify the service account
           }
         }
       }
     ]
   })
 }
+  # This is the standard managed policy for the EBS CSI driver
+resource "aws_iam_policy_attachment" "ebs_csi_driver_policy_attachment" {
+   name  = aws_iam_role.ebs-csi-driver-role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
 
-
-#resource "aws_iam_policy_attachment" "ebs_csi_driver_policy_attachment" {
- # name  = aws_iam_role.ebs-csi-driver-role.role
- # policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-#}
 
 resource "aws_iam_role" "eks_nodegroup_role" {
       name = "eks-nodegroup-role"
