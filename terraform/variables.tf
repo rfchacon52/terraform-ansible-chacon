ef860a711c2da5ed2fbad59d5603e29b15eddd48
rfchacon52 <rfchacon52@gmail.com>
2025-05-21 18:15:43 -0700

    update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,7 +3,7 @@
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = EKS-AM-Cluster" # Customize this
+  default     = "EKS-blueprintsr" # Customize this
 }
 
 variable "region" {
