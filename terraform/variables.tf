55976cede231fd3d7bd7522a6b3b9544eebb41df
rfchacon52 <rfchacon52@gmail.com>
2025-05-31 01:07:23 -0700

    ok

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,7 +12,7 @@ variable "cluster_name" {
 variable "ami_type" {
   description = "Default EKS AMI release version for node groups"
   type        = string
-  default     = "amazon-eks-node-al2023-x86_64-standard-1.31-v20250519" 
+  default     = "AL2023_x86_64_STANDARD" 
 }
 
 variable "instance_type" {
