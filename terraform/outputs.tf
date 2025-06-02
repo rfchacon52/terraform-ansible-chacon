4891ee3ae98c450c03fde07ee6892a124eefb02e
rfchacon52 <rfchacon52@gmail.com>
2025-06-01 18:00:42 -0700

    update

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -25,18 +25,18 @@ output "eks_cluster_endpoint" {
   value       = module.eks.cluster_endpoint
 }
 
-output "aws_load_balancer_controller_irsa_arn" {
-  description = "ARN of the IAM Role for Service Account (IRSA) for AWS Load Balancer Controller."
-  value       = module.eks_blueprints_addons.aws_load_balancer_controller_irsa_arn
-}
+#output "aws_load_balancer_controller_irsa_arn" {
+#  description = "ARN of the IAM Role for Service Account (IRSA) for AWS Load Balancer Controller."
+#  value       = module.eks_blueprints_addons.aws_load_balancer_controller_irsa_arn
+#}
 
-output "application_ingress_hostname" {
-  description = "Hostname of the AWS Application Load Balancer created by the Ingress resource."
-  value = one(kubernetes_ingress_v1.my_application_ingress.status[0].load_balancer[0].ingress[0].hostname)
-}
+#output "application_ingress_hostname" {
+#  description = "Hostname of the AWS Application Load Balancer created by the Ingress resource."
+#  value = one(kubernetes_ingress_v1.my_application_ingress.status[0].load_balancer[0].ingress[0].hostname)
+#}
 
-output "application_ingress_ip" {
-  description = "IP address of the AWS Application Load Balancer created by the Ingress resource (if applicable)."
-  value = try(one(kubernetes_ingress_v1.my_application_ingress.status[0].load_balancer[0].ingress[0].ip), "N/A")
-}
+#output "application_ingress_ip" {
+#  description = "IP address of the AWS Application Load Balancer created by the Ingress resource (if applicable)."
+#  value = try(one(kubernetes_ingress_v1.my_application_ingress.status[0].load_balancer[0].ingress[0].ip), "N/A")
+#}
 
