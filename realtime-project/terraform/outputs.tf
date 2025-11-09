038aad410a93fbba82d67e42cc4a580504f9e826
rfchacon52 <rfchacon52@gmail.com>
2025-11-09 10:13:42 -0800

    update

---
+++ realtime-project/terraform/outputs.tf
@@ -0,0 +1,9 @@
+output "vpc_id" {
+  description = "The ID of the VPC created by the module"
+  value       = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value       = module.vpc.private_subnets
+}
