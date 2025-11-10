368738653d4ca7c7ff28002998d5343d092fed84
rfchacon52 <rfchacon52@gmail.com>
2025-11-09 17:23:35 -0800

    update

--- realtime-project/terraform/outputs.tf
+++ realtime-project/terraform/outputs.tf
@@ -7,15 +7,3 @@ output "private_subnets" {
   description = "List of IDs of private subnets"
   value       = module.vpc.private_subnets
 }
-
-
-output "ingress_external_url" {
-  description = "The external URL/CNAME of the NGINX Ingress Controller's Load Balancer"
-  # This retrieves the hostname of the AWS Load Balancer created by the Ingress Service
-  value = one(kubernetes_service_v1.ingress_nginx.status[0].load_balancer[0].ingress[*].hostname)
-}
-
-output "ingress_service_ip" {
-  description = "The External IP address of the NGINX Ingress Controller Service (may be null for NLB CNAME)"
-  value = one(kubernetes_service_v1.ingress_nginx.status[0].load_balancer[0].ingress[*].ip)
-}
