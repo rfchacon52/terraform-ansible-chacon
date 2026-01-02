372b081aec9c367e222c5a622bb4a2a21f229253
rfchacon52 <rfchacon52@gmail.com>
2026-01-01 23:13:50 -0800

    update

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -11,18 +11,6 @@ required_version = ">= 1.12.0" # Or your desired minimum Terraform version
       source  = "hashicorp/aws"
       version = "5.98.0"
     }
-   kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "2.27.0"
-    }
-  kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = "1.14.0"
-    }
-  helm = {
-      source  = "hashicorp/helm"
-      version = "2.17.0"
-    }
 
   }
 
