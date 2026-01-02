2cdc4946d0c9b9624d239cd9287ae3c9f6ed08f2
rfchacon52 <rfchacon52@gmail.com>
2026-01-01 22:47:04 -0800

    update

--- auto-mode/providers.tf
+++ auto-mode/providers.tf
@@ -4,24 +4,11 @@
 
 terraform {
 
-required_version = ">= 1.12.0" # Or your desired minimum Terraform version
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "5.83.0"
-    }
-   kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "2.27.0"
-    }
-  kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = "1.14.0"
-    }
-  helm = {
-      source  = "hashicorp/helm"
-      version = "2.17.0"
     }
 
   }
@@ -41,9 +28,7 @@ required_version = ">= 1.12.0" # Or your desired minimum Terraform version
 
 }
 
-
 provider "aws" {
   region = "us-east-1"
-  alias  = "virginia"
 }
 
